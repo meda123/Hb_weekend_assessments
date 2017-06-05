694e3ff0acb0c90ed7e83c261a554a9ba5664374
meda123 <mrt62@cornell.edu>
2017-06-05 13:56:32 -0700

    Completed OO questions.

--- w2_object_orientation/assessment.py
+++ w2_object_orientation/assessment.py
@@ -4,20 +4,69 @@ Part 1: Discussion
 1. What are the three main design advantages that object orientation
    can provide? Explain each concept.
 
+   The three main design advantages of object orientation include abstraction, 
+   polymorphism, and encapsulation. 
+
+   Abstraction affords programmers the ability to hide certain pieces of their
+   code from the interface that other programmers or users interact with. A 
+   programmer using a class you created does not need to understand each detail
+   of the class in order to use it, thus reducing the complexity. 
+
+   Polymorphism allows for a group to share interchangeable functionality so that
+   components that do the same kind of things can be used interchangeable.
+
+    Encampsulation means that methods pertaining to certain parts of a program
+    stays associated, or "belong to", that part of the program. For example, all
+    of the functionality of an exam can travel in an Exam class - import that,
+    and you get all of the parts.     
+
+
+
 2. What is a class?
 
+    A class is a blueprint to make an object (aka instances). Note, a class is
+    not something in it of itself, but rather a map to build a particular data
+    structure (e.g. linked lists, trees, stacks, etc. 
+
+
 3. What is an instance attribute?
+    At a high level, an instance attribute is a variable inside a class. It is
+    a piece of data (which can be any type, such as a list, integer, etc) that 
+    is assinged to each individual instance. 
+
+    For example, for a Human class, we could have instance attributes for those 
+    things that are different person to person, like eye color, name, etc.
+
 
 4. What is a method?
+    A method isa a function inside a class. A method is like a behavior that a 
+    class can execute. 
+
 
 5. What is an instance in object orientation?
+    An instance is an individual occurrence of an object, the idea made manifest.
+    For an Exam class, each individual exam (e.g. Unit Test 1) would be an instance.
+
 
 6. How is a class attribute different than an instance attribute?
    Give an example of when you might use each.
 
+    A class attribute exists on a class, whereas instance attributes exist on 
+    the individual instances, and therefore their values may vary by instance.
+
+    Example: Class attribute 
+    In a Cat class, it would make sense to have a class attribute 
+    of species (which would have the value Felis catus for all Cat instances).
+    
+    Example: Instance attribute
+    In a Cat class, and instance attribute of name (which would be different for
+    each Cat instance) would be better suited than a class attribute. 
 
 """
 
 
 # Parts 2 through 5:
 # Create your classes and class methods
+
+
+
